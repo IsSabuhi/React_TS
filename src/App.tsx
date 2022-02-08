d1e83ae8a27f4b3ff7f56276771c66f1f5ce8697
IsSabuhi <sabuhi.israfilov2001@gmail.com>
2022-02-09 00:08:00 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import { ITodo } from './components/interfaces';
+import { Navbar } from './components/Navbar';
+import { TodoForm } from './components/TodoForm';
+import { TodoList } from './components/TodoList';
+
+declare var confirm: (question: string) => boolean
 
 function App() {
+
+  const [todos, setTodos] = useState<ITodo[]>([])
+
+  useEffect(() => {
+    const saved = JSON.parse(localStorage.getItem('todos') || '[]') as ITodo[]
+    setTodos(saved)
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }, [todos])
+
+  const addHandler = (title: string) => {
+    const newTodo: ITodo = {
+      title: title,
+      id: Date.now(),
+      completed: false
+    }
+    // setTodos([newTodo, ...todos])
+    setTodos(prev => [newTodo, ...prev])
+  }
+
+  const toggleHandler = (id: number) => {
+    setTodos(prev => prev.map(todo => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo
+    }))
+  }
+
+  const removeHandler = (id: number) => {
+    const shoudRemove = confirm("Вы уверены, что хотите удалить элемент?")
+    if (shoudRemove) {
+      setTodos(prev => prev.filter(todo => todo.id !== id))
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Navbar />
+    <div className='container'>
+
+      <TodoForm onAdd={addHandler} />
+
+      <TodoList 
+      todos={todos}
+      onToggle={toggleHandler}
+      onRemove={removeHandler}
+      />
+
     </div>
+    </>
   );
 }
 
---
+++ src/components/TodoForm.tsx
@@ -0,0 +1,46 @@
+import React, {useRef} from "react";
+
+
+interface TodoFormProps {
+    onAdd(title: string): void
+}
+
+
+export const TodoForm: React.FC<TodoFormProps> = props => {
+
+    // Первый метод
+    // const [title, setTitle] = useState<string>('')
+
+    // const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    //     setTitle(event.target.value)
+    // }
+
+
+    // Второй метод
+    const ref = useRef<HTMLInputElement>(null)
+
+
+    const keyPressHandler = (event: React.KeyboardEvent) => {
+        if (event.key === "Enter") {
+            props.onAdd(ref.current!.value)
+            ref.current!.value = ""
+            // console.log(title)
+            // setTitle("")
+        }
+    }
+
+    return (
+        <div className="input-field mt2">
+            <input 
+            // onChange={changeHandler}
+            // value={title} 
+            ref={ref}
+            type="text" 
+            id="title" 
+            placeholder="Введите название дела"
+            onKeyPress={keyPressHandler}
+            />
+            <label htmlFor="title" className="active">Название дела</label>
+        </div>
+    )
+}
